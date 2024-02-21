660b0090faf47da13ab2b6c8b12cfec0e62c1d49
tiri39 <etisadiaafrin@gmail.com>
2024-02-22 00:50:48 +0600

    added emergency location sharing option

---
+++ lib/widgets/home_widgets/safehome/SafeHome.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class SafeHome extends StatelessWidget{
+  
+  showSafeHome(BuildContext context){
+    showModalBottomSheet(
+      context: context, 
+      builder: (context){
+        return Container(
+          height: MediaQuery.of(context).size.height/1.4,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),),
+          ),
+        );
+    },
+    );
+  }
+
+
+  @override 
+  Widget build(BuildContext context)
+  {
+    return InkWell(
+      onTap: () => showSafeHome(context),
+      child: Card(
+        elevation: 5,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Container(
+          height: 180,
+          width: MediaQuery.of(context).size.width * 0.7,
+          decoration: BoxDecoration(
+      
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    ListTile(
+                      title: Text("Send Location"),
+                      subtitle: Text("Share Location"),
+                    ),
+                  ],
+                ),),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset('assets/location.png'))
+            ],),
+        ),
+      ),
+    );
+  }
+}
