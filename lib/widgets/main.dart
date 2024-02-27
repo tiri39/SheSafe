94c9dcf456e327f020dbe566f6cb4ce05afec55f
tiri39 <etisadiaafrin@gmail.com>
2024-02-28 00:19:40 +0600

    added firebase

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:shesafe/widgets/home_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shesafe/child/child_login_screen.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
